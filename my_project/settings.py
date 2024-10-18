858fe0ca21dddb4341449f63d33b4d01f8873cd5
Godwin Naces <godwinnaces@gmail.com>
2024-10-18 13:13:22 +0000

    set debug to True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gzeka9gy$l3b#goc8e=97hrwv-rx(xov6$vun#*(d^1q)h4ve2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-gnaces-djangoproject-cwure07upzo.ws.codeinstitute-ide.net','.herokuapp.com']
 
